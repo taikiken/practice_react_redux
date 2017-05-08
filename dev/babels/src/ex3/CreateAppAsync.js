229c275b8e1a14f135b9e981338954d76618fda3
taikiken <o@parachute-canopy.org>
2017-05-08 19:52:54 +0900

    redx-thunk try

---
+++ dev/babels/src/ex3/CreateAppAsync.js
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2011-2017 inazumatv.com, inc.
+ * @author (at)taikiken / http://inazumatv.com
+ * @date 2017/05/08 - 19:50
+ *
+ * Distributed under the terms of the MIT license.
+ * http://www.opensource.org/licenses/mit-license.html
+ *
+ * This notice shall be included in all copies or substantial portions of the Software.
+ *
+ */
